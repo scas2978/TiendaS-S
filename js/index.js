ca6ac657e7a8923a062d87583d16010cbe6d300f
scas2978 <santi007374234@outlook.com>
2023-02-24 14:42:54 -0500

    js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const hamburguer =document.querySelector('.hamburguer');
+const menu=document.querySelector('.menu-navegacion');
+
+
+console.log(menu)
+console.log(hamburguer)
+
+hamburguer.addEventListener('click',()=>{
+
+})
