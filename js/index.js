6e84b97706ccb2243d3d6408f53b19e8ac04ce61
scas2978 <santi007374234@outlook.com>
2023-02-24 15:10:00 -0500

    js2

--- js/index.js
+++ js/index.js
@@ -6,5 +6,16 @@ console.log(menu)
 console.log(hamburguer)
 
 hamburguer.addEventListener('click',()=>{
+menu.classList.toggle("spread")
+})
 
+
+window.addEventListener('click',e=>{
+  if(menu.classList.contains('spread')
+     && e.target!=menu &&e.target!=hamburguer){
+
+        
+        menu.classList.toggle("spread")
+
+  }
 })
